986000d489d0bc11b1099a0d65c76e1fe74c500f
Gautham Vijayan <gauthamnvj@gmail.com>
2025-05-30 17:48:03 +0530

    utils,exception and logger added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'Gautham N Vijayan',
+    author_email= 'gauthamnvj@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
