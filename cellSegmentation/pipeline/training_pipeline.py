99e19e0340a7dbf0adf677273b826d09cc8265f3
Gautham Vijayan <gauthamnvj@gmail.com>
2025-05-31 17:17:03 +0530

    data ingestion  done

---
+++ cellSegmentation/pipeline/training_pipeline.py
@@ -0,0 +1,64 @@
+import sys, os
+from cellSegmentation.logger import logging
+from cellSegmentation.exception import AppException
+from cellSegmentation.components.data_ingestion import DataIngestion
+# from cellSegmentation.components.data_validation import DataValidation
+# from cellSegmentation.components.model_trainer import ModelTrainer
+
+from cellSegmentation.entity.config_entity import (
+    DataIngestionConfig,
+    # DataValidationConfig,
+    # ModelTrainerConfig
+)
+
+from cellSegmentation.entity.artifacts_entity import (
+    DataIngestionArtifact,
+    # DataValidationArtifact,
+    # ModelTrainerArtifact
+)
+
+
+class TrainPipeline:
+    def __init__(self):
+        self.data_ingestion_config = DataIngestionConfig()
+        # self.data_validation_config = DataValidationConfig()
+        # self.model_trainer_config = ModelTrainerConfig()
+
+    def start_data_ingestion(self) -> DataIngestionArtifact:
+        try:
+            logging.info("Entered the start_data_ingestion method of TrainPipeline class")
+            logging.info("Getting the data from URL")
+
+            data_ingestion = DataIngestion(
+                data_ingestion_config=self.data_ingestion_config
+            )
+
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
+            logging.info("Got the data from URL")
+            logging.info("Exited the start_data_ingestion method of TrainPipeline class")
+
+            return data_ingestion_artifact
+
+        except Exception as e:
+            raise AppException(e, sys)
+
+    def run_pipeline(self) -> None:
+        try:
+            data_ingestion_artifact = self.start_data_ingestion()
+
+            # Uncomment and use this block when you implement data validation
+            # data_validation_artifact = self.start_data_validation(
+            #     data_ingestion_artifact=data_ingestion_artifact
+            # )
+
+            # if data_validation_artifact.validation_status:
+            #     model_trainer_artifact = self.start_model_trainer()
+            # else:
+            #     raise Exception("Your data is not in correct format")
+
+            # Placeholder until data validation is implemented
+            logging.warning("Skipping model training because data validation is not implemented.")
+
+        except Exception as e:
+            raise AppException(e, sys)
